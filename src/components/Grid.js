799f36c7b4a155351ad956ef96c29f3443e4f983
Joel Hans <j@joelhans.com>
2023-06-08 09:18:31 -0700

    Add external link icon (#245)

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from '@docusaurus/Link';
+import isInternalUrl from '@docusaurus/isInternalUrl';
+import ExternalLink from '@theme/Icon/ExternalLink';
 import styles from './Grid.module.css';
 
 export default function Grid ({ children }) {
@@ -12,10 +14,17 @@ export default function Grid ({ children }) {
 
 export function Button ({ children, style = {}, href, title, cta }) {
   return (
+    <>
     <Link to={href} className={styles.button} style={style}>
+      {!isInternalUrl(href) && (
+        <div className={styles.ext}>
+          <ExternalLink />
+        </div>
+      )}
       {title && <h3>{title}</h3>}
       {children && <p>{children}</p>}
       {cta && <p className={styles.cta}>{cta} &rarr;</p>}
     </Link>
+    </>
   );
 }
